e40e9ada218bdd634373f056e2f0773f3146b72c
Bruno Coelho <bbcoelho@gmail.com>
2025-10-03 23:04:12 -0300

    feat: add saveParkedCar to IParkingLotDataAccess

--- src/core/dataAccess/IParkingLotDataAccess.ts
+++ src/core/dataAccess/IParkingLotDataAccess.ts
@@ -2,4 +2,5 @@ import ParkingLot from "../entities/ParkingLot.js";
 
 export default interface IParkingLotDataAccess {
     getParkingLot(code: string): ParkingLot;
+    saveParkedCar(code: string, plate: string, date: Date): void;
 }
