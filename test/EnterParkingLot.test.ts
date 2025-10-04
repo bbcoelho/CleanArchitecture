8ac6c193065339c1331e3098b612cf148678bfa3
Bruno Coelho <bbcoelho@gmail.com>
2025-10-03 22:13:43 -0300

    test: EnterParkingLot with ParkingLotRepositoryMemory

--- test/EnterParkingLot.test.ts
+++ test/EnterParkingLot.test.ts
@@ -1,5 +1,10 @@
 import { test, expect } from "@jest/globals";
+import { EnterParkingLot } from "../src/core/usecases/EnterParkingLot.js";
+import ParkingLotRepositoryMemory from "../src/core/infra/repositories/ParkingLotRepositoryMemory.js";
 
 test("EnterParkingLot", () => {
-  expect(1).toBe(1);
+	const parkingLotRepository = new ParkingLotRepositoryMemory();
+	const enterParkingLot = new EnterParkingLot(parkingLotRepository);
+	const parkingLot = enterParkingLot.execute('shopping');
+	expect(parkingLot.getCode()).toBe("shopping");
 });
