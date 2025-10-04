54226a0f64184ecfc140c48139ac82feb007a947
Bruno Coelho <bbcoelho@gmail.com>
2025-10-03 21:54:07 -0300

    feat: add ParkingLotEntity

---
+++ src/core/entities/ParkingLot.ts
@@ -0,0 +1,29 @@
+export default class ParkingLot {
+    private code: string;
+    private capacity: number;
+    private openHour: number;
+    private closeHour: number;
+
+    constructor(code: string, capacity: number, openHour: number, closeHour: number) {
+        this.code = code;
+        this.capacity = capacity;
+        this.openHour = openHour;
+        this.closeHour = closeHour;
+    }
+
+    public getCode(): string {
+        return this.code;
+    }
+
+    public getCapacity(): number {
+        return this.capacity;
+    }
+    
+    public getOpenHour(): number {
+        return this.openHour;
+    }
+
+    public getCloseHour(): number {
+        return this.closeHour;
+    }
+}
