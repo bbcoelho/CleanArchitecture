e1e9ad9e25532405671d871476b59e3408fb4cec
Bruno Coelho <bbcoelho@gmail.com>
2025-10-03 21:28:49 -0300

    chore: Jest setup with Typescript

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+      isolatedModules: true
+    }]
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@jest/transform|ts-jest)/)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts'
+  ]
+};
---
+++ test/EnterParkingLot.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "@jest/globals";
+
+test("EnterParkingLot", () => {
+  expect(1).toBe(1);
+});
